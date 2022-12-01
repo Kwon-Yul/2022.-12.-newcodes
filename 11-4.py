276f483ac2f6778f752aa057394d0b9429683e12
권율 <rnjsdbf95@gmail.com>
2022-12-01 15:07:12 +0900

    asdf

---
+++ 11-4.py
@@ -0,0 +1,34 @@
+import numpy as np
+aa = np.zeros((2,3))
+ab = np.ones((2,3))
+ac = np.full((2,3),100)
+ad = np.eye(3)
+ae = np.random.random((2,3))
+af = np.arange(0,10)
+ag = np.arange(0,10,2)
+ah = np.arange(0,10,3)
+ai = np.arange(0.0,1.0,0.2)
+aj = np.linspace(0,10,5)
+ak = np.linspace(0,10,4)
+print(aa)
+print(ab)
+print(ac)
+print(ad)
+print(ae)
+print(ah)
+print(ai)
+print(aj)
+print(ak)
+
+#1.
+a1 = np.full((3,3),2)
+print(a1)
+#2
+a2 = np.arange(1,12,)
+print(a2)
+#3
+a3 = np.arange(3,50,3)
+print(a3)
+#4
+a4 = np.linspace(0,20,5)
+print(a4)
